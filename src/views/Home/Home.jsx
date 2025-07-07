22cbc0318b1872dd3a5abd715083ac19ab398330
DivyaAhire11 <divyaahire121@gmail.com>
2025-07-07 14:34:03 +0530

    basic setup of restaurant website

---
+++ src/views/Home/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import MultipleItems from "../../assets/MultipleItems.jpg"
+
+const Home = () => {
+  return (
+   <>  
+       <div className="main-container">
+        <div className="hero-section">
+           <img src={MultipleItems} alt="hero" />
+        </div>
+
+
+
+        
+        </div>
+   </>
+  )
+}
+
+export default Home
