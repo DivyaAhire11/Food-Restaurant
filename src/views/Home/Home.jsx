16ace40e2f0c467f11cb8874092a9524b23cbe99
DivyaAhire11 <divyaahire121@gmail.com>
2025-08-06 23:43:20 +0530

    some changes added

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,29 +1,34 @@
 import React from 'react'
-import MultipleItems from "../../assets/MultipleItems.jpg"
-import burgerImages from "../../assets/burgerImages.jpg"
-import Chakli from "../../assets/chakli.jpg"
 import Navbar from '../../Components/Navbar/Navbar'
-import food1 from '../../assets/food1.jpg'
+import gulabjamun from '../../assets/gulabjamun.webp'
+import vadaRecipe from "../../assets/vadaRecipe.jpg"
+import Khamang from "../../assets/Khamang.webp"
+
 import "./home.css"
 
 const Home = () => {
   return (
    <>  
-        
+         {/* <img src= {gulabjamun} alt="dish" className='gulabJamun'/>      */}
+       
         <div className="hero-section">
-           <Navbar/>
-           <img src={food1} alt="main" />
+           <Navbar/> 
+           {/* <img src={vadaRecipe} alt='vadarecipies' className='vadarecipies'/> */}
+           {/* <img src={food1} alt="main" /> */}
+           <img src ={Khamang} />
           <div className='hero-Details'>
+               
                <h1 className="main-heading">best food for <br/> your taste</h1>
                <p className='msg'>Italian food makes people think og big family dinners. So you can many want to positin your restaurant as a place to bring the whole family</p>
            
             <button className='btn'>Read More</button>
+              
           </div>
          
         </div>
 
           <div className="categories">
-            <img src={ Chakli} className='burger'/>
+           
           </div>
 
 
